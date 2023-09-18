88bfd9afce2a3052ab6df3400a0d63592c67d041
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2023-09-18 14:37:22 +0530

    fix gnu plot generation

--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 import subprocess
 import argparse
 
-
 # parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("flutter_type", type=int)
@@ -26,26 +25,28 @@ else:
             'code.txt', 'TF_code.txt']
     subprocess.run(p)
 
-p = subprocess.Popen(['gnuplot','-p'], shell=True, stdin=subprocess.PIPE,)
+p = subprocess.Popen(['gnuplot','-p'], shell=True, stdin=subprocess.PIPE)
 p.stdin.write(b'set lmargin 10 ; set rmargin 10\n')
 p.stdin.write(b'set xlabel "k" \n')
 p.stdin.write(b'set ylabel "Gain" \n')
 p.stdin.write(b'set sample 1000  \n')
 p.stdin.write(b'set terminal png \n')
-p.stdin.write(b'set output code.png \n')
+p.stdin.write(b'set output "code.png" \n')
 p.stdin.write(b'set key on  below box title "Legend" \n')
-p.stdin.write(b'set key left Left reverse \n ' )
+p.stdin.write(b'set key left Left reverse \n')
 p.stdin.write(b'plot "code.txt" with steps title "Flutter shutter function" linewidth 1.5 \n')
 p.stdin.write(b'exit \n')
+p.communicate()
 
-p = subprocess.Popen(['gnuplot','-p'], shell=True, stdin=subprocess.PIPE,)
+p = subprocess.Popen(['gnuplot','-p'], shell=True, stdin=subprocess.PIPE)
 p.stdin.write(b'set lmargin 10 ;  set rmargin 10 \n')
 p.stdin.write(b'set xlabel "xi" \n')
 p.stdin.write(b'set ylabel "Fourier tranform (modulus)" \n')
 p.stdin.write(b'set sample 1000  \n')
 p.stdin.write(b'set terminal png \n')
-p.stdin.write(b'set output TF_code.png \n')
+p.stdin.write(b'set output "TF_code.png" \n')
 p.stdin.write(b'set key on  below box title "Legend" \n')
 p.stdin.write(b'set key left Left reverse \n')
 p.stdin.write(b'plot "TF_code.txt" using 1:2 with lines title "Fourier transform (modulus) of the flutter function" linewidth 1.5 \n')
-
+p.stdin.write(b'exit \n')
+p.communicate()
